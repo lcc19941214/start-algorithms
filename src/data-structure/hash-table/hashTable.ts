ecfbddbf708302bf632d34efa24c228bbc40dafb
lcc19941214 <245987239@qq.com>
2019-04-14 22:37:28 +0800

    feat: update HashTable & add tests

--- src/data-structure/hash-table/hashTable.ts
+++ src/data-structure/hash-table/hashTable.ts
@@ -7,24 +7,24 @@ interface BucketItem<T = any> {
   value: T;
 }
 
-export default class HashTable<T> {
+export default class HashTable<T = any> {
   private keys: { [key: string]: number };
-  private buckets: LinkedList<BucketItem<T>>[];
+  public buckets: LinkedList<BucketItem<T>>[];
   private hashGenerator?: hashGenerator;
 
   public constructor(size = DEFAULT_BUCKETS_SIZE, hashGenerator?: hashGenerator) {
     this.keys = {};
-    this.buckets = [...Array(size)].map(() => new LinkedList<BucketItem<T>>());
-    this.hashGenerator = hashGenerator;
+    this.buckets = Array.from(Array(size)).map(() => new LinkedList<BucketItem<T>>());
+    this.hashGenerator = hashGenerator || this.hash;
   }
 
   private hash(key: string): number {
-    const hash = key;
-    return hash.length;
+    const hash = Array.from(key).reduce((code, str) => code + str.charCodeAt(0), 0);
+    return hash % this.buckets.length;
   }
 
   public set(key: string, value: T): T {
-    const hash = this.hash(key);
+    const hash = this.hashGenerator(key);
     this.keys[key] = hash;
     const bucket = this.buckets[hash];
     const node = bucket.find(node => node.value.key === key);
@@ -40,11 +40,36 @@ export default class HashTable<T> {
     return value;
   }
 
-  public get(key: string) {}
+  public get(key: string) {
+    const hash = this.keys[key];
+    const bucket = this.buckets[hash];
+    if (bucket) {
+      const node = bucket.find(node => node.value.key === key);
+      if (node) {
+        return node.value.value;
+      }
+    }
 
-  public delete(key: string): boolean {}
+    return undefined;
+  }
 
-  public has(key: string): boolean {}
+  public delete(key: string): boolean {
+    const hash = this.keys[key];
+    const bucket = this.buckets[hash];
+    if (bucket) {
+      const node = bucket.find(node => node.value.key === key);
+      if (node) {
+        delete this.keys[key];
+        return bucket.remove(node.value);
+      }
+    }
+
+    return false;
+  }
+
+  public has(key: string): boolean {
+    return Object.prototype.hasOwnProperty.call(this.keys, key);
+  }
 
   public getKeys(): string[] {
     return Object.keys(this.keys);
